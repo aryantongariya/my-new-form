cdec73fcae49d02f2ba1f54751f53ee92eddec99
aryantongariya <84620318+aryantongariya@users.noreply.github.com>
2025-02-12 13:05:15 +0530

    Update Login.js

--- Frontend/src/components/Login.js
+++ Frontend/src/components/Login.js
@@ -7,7 +7,7 @@ const Login = (props) => {
     let history = useHistory()
     const handleSubmit = async (e)=>{
         e.preventDefault()
-        const response = await fetch("http://localhost:8800/api/auth/login", { 
+        const response = await fetch("https://my-new-form-1.onrender.com/api/auth/login", { 
             method: 'POST',   
             headers: {
               'Content-Type': 'application/json'
